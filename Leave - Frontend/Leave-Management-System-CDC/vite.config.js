f5393f70b248322f234635e46da429cc336670a2
nnuwansa <159515255+nnuwansa@users.noreply.github.com>
2026-01-09 17:07:19 +0530

    Updated vite.config,js

--- Leave - Frontend/Leave-Management-System-CDC/vite.config.js
+++ Leave - Frontend/Leave-Management-System-CDC/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    port: 5173,
+    host: true,
+    strictPort: false,
+  },
+
+ 
+  optimizeDeps: {
+    force: true,
+  },
+
+  // Clear the cache directory
+  cacheDir: "node_modules/.vite",
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
